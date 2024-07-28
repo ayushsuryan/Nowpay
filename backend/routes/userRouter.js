fa2ba204c2cc17791453575b0f0a00fb0253042b
Ayush Suryan <suryanayush@gmail.com>
2024-07-28 08:09:10 +0530

    fixed user Info update method

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -108,10 +108,7 @@ router.put("/update", authMiddleware, async (req, res) => {
     return res.status(411).json({ message: "Invalid Inputs" });
   }
   console.log(req.userId);
-  await userModel.updateOne(
-    { _id: req.userId },
-    { $set: { firstName: "Ayush" } }
-  );
+  await userModel.updateOne({ _id: req.userId }, { $set: req.body });
   res.status(200).json({
     message: "Updated user information.",
   });
