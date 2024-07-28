d0c02c2778a47f862cc76d042b0d84ab414433c6
Ayush Suryan <suryanayush@gmail.com>
2024-07-28 08:10:21 +0530

    removed logs

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -107,7 +107,6 @@ router.put("/update", authMiddleware, async (req, res) => {
   if (!success) {
     return res.status(411).json({ message: "Invalid Inputs" });
   }
-  console.log(req.userId);
   await userModel.updateOne({ _id: req.userId }, { $set: req.body });
   res.status(200).json({
     message: "Updated user information.",
